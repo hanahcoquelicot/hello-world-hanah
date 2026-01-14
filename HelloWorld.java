8c375232a1851481e54fecea2c46840e279d7ade
Hanah <245273718+hanahcoquelicot@users.noreply.github.com>
2026-01-14 13:40:49 -0500

    display comment

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main (String{} args) {
-        // Shout out message "Hello, World"
+        // Display message "Hello, World"
         System.out.println("Hello, World!")
     }
 }
