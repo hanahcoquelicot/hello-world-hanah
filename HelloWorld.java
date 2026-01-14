743916196d0aa0ef699e01b7f61fdf110e469e0b
Hanah <245273718+hanahcoquelicot@users.noreply.github.com>
2026-01-14 13:25:16 -0500

    update comment in java hello

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main (String{} args) {
-        // Welcome message "Hello, World"
+        // Shout out message "Hello, World"
         System.out.println("Hello, World!")
     }
 }
