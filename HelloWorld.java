8996631c334bbb0c48bb21467b9432472712f79e
Nadia <11464667+nadiafawaz@users.noreply.github.com>
2026-01-14 13:29:53 -0500

    super message in java hello

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main (String{} args) {
-        // Welcome with "Hello, World"
+        // Super welcome with "Hello, World"
         System.out.println("Hello, World!")
     }
 }
