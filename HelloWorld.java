47a2f19710bbfa0b619cc0ecf847f82fa3fdff7e
Hanah <245273718+hanahcoquelicot@users.noreply.github.com>
2026-01-14 13:46:27 -0500

    add punctuation

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,6 @@
 public class HelloWorld {
     public static void main (String{} args) {
-<<<<<<< HEAD
-        // Display message "Hello, World"
-=======
-        // Super welcome with "Hello, World"
->>>>>>> e90f9ad49fe7aebccfd513a7cb23e9ee12e2adc7
+        // Display message "Hello, World!"
         System.out.println("Hello, World!")
     }
 }
